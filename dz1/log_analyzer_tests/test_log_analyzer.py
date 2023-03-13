4d5bcf2cd3a763fcd7fc1b8c7610441a65070a40
Алексей А. Абрамов <aa.abramov@jet.su>
2023-03-13 16:53:23 +0300

    fixes for linters

--- dz1/log_analyzer_tests/test_log_analyzer.py
+++ dz1/log_analyzer_tests/test_log_analyzer.py
@@ -43,7 +43,7 @@ def test_load_external_config():
 
     expected = copy.copy(config)
     print(f'!!!{os.listdir(os.getcwd())}!!!')
-    with open(f'{os.getcwd()}dz1//log_analyzer/config.json',
+    with open(f'{os.getcwd()}/dz1/log_analyzer/config.json',
               encoding='utf-8') as file:
         expected.update(json.load(file))
 
