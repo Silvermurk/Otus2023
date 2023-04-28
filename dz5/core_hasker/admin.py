cf9c9ca6b1ea5903e4b733ded4d6da74b653fd24
Алексей А. Абрамов <aa.abramov@jet.su>
2023-04-28 22:50:25 +0300

    linter

--- dz5/core_hasker/admin.py
+++ dz5/core_hasker/admin.py
@@ -1,25 +1,37 @@
-from django.contrib import admin
-
-from .models import Question, Answer
 """
 Admin page for django project
 """
 
+from django.contrib import admin
+
+from dz5.core_hasker.models import Question, Answer
+
 
 class QuestionAdmin(admin.ModelAdmin):
+    """
+    Admin question form
+    """
     list_display = ('title', 'author', 'created_at')
     prepopulated_fields = {'slug': ('title',)}
 
 
 class AnswerAdmin(admin.ModelAdmin):
-    pass
+    """
+    Admin answer form
+    """
 
 
 class SavedQuestionAdmin(admin.ModelAdmin):
+    """
+    Saved Qestion admin form
+    """
     list_display = ('user', 'question',)
 
 
 class QuestionTagAdmin(admin.ModelAdmin):
+    """
+    Tag admin form
+    """
     list_display = ('name',)
     prepopulated_fields = {'slug': ('name',)}
 
