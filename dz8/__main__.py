8d2fea94fe1708102159c361c989086bd616273b
Silvermurk <silvermurk@gmail.com>
2023-07-03 21:03:49 +0300

    dz 8

--- dz8/__main__.py
+++ dz8/__main__.py
@@ -1,3 +1,4 @@
+# pylint:disable=deprecated-module
 import glob
 import gzip
 import logging
@@ -17,7 +18,9 @@ import memcache
 from . import appsinstalled_pb2
 from .types import AppsInstalled, ProcessingStatus
 
-
+"""
+Main module for memtest, environment variables set here
+"""
 NORMAL_ERR_RATE = 0.01
 MEMCACHE_RETRY_NUMBER = 3
 MEMCACHE_RETRY_TIMEOUT_SECONDS = 1
@@ -25,25 +28,30 @@ MEMCACHE_SOCKET_TIMEOUT_SECONDS = 3
 
 
 def dot_rename(path):
-    head, fn = os.path.split(path)
-    # atomic in most cases
-    os.rename(path, os.path.join(head, "." + fn))
+    """
+    Dot split os path to remove dots in path sirectorys
+    """
+    head, function = os.path.split(path)
+    os.rename(path, os.path.join(head, "." + function))
 
 
 def insert_appsinstalled(
-    memcache_client: memcache.Client,
-    appsinstalled: AppsInstalled,
-    dry_run: bool = False,
-) -> bool:
-    ua = appsinstalled_pb2.UserApps()
-    ua.lat = appsinstalled.lat
-    ua.lon = appsinstalled.lon
-    key = "%s:%s" % (appsinstalled.dev_type, appsinstalled.dev_id)
-    ua.apps.extend(appsinstalled.apps)
-    packed = ua.SerializeToString()
+        memcache_client: memcache.Client,
+        appsinstalled: AppsInstalled,
+        dry_run: bool = False,
+        ) -> bool:
+    """
+    Memcache main method to insert data
+    """
+    user_apps = appsinstalled_pb2.UserApps()
+    user_apps.lat = appsinstalled.lat
+    user_apps.lon = appsinstalled.lon
+    key = "%s:%s", (appsinstalled.dev_type, appsinstalled.dev_id)
+    user_apps.apps.extend(appsinstalled.apps)
+    packed = user_apps.SerializeToString()
 
     if dry_run:
-        logging.debug("%s -> %s" % (key, str(ua).replace("\n", " ")))
+        logging.debug("%s -> %s", (key, str(user_apps).replace("\n", " ")))
         return True
 
     for _ in range(MEMCACHE_RETRY_NUMBER):
@@ -52,9 +60,9 @@ def insert_appsinstalled(
             # https://github.com/linsomniac/python-memcached/blob/bad41222379102e3f18f6f2f7be3ee608de6fbff/memcache.py#L698
             success: bool = memcache_client.set(
                 (appsinstalled.dev_type.value, key), packed
-            )
-        except Exception as e:
-            logging.exception(f"Cannot write to Memcache: {e}")
+                )
+        except Exception as exception:
+            logging.exception(f"Cannot write to Memcache: {exception}")
             return False
         if success:
             return True
@@ -65,8 +73,11 @@ def insert_appsinstalled(
 
 
 def process_line(
-    raw_line: bytes, memcache_client: memcache.Client, dry: bool
-) -> ProcessingStatus:
+        raw_line: bytes, memcache_client: memcache.Client, dry: bool
+        ) -> ProcessingStatus:
+    """
+    Process single line by memcache
+    """
 
     line = raw_line.decode("utf-8").strip()
     if not line:
@@ -78,14 +89,17 @@ def process_line(
         logging.error(f"Cannot parse line: {e}")
         return ProcessingStatus.ERROR
 
-    ok: bool = insert_appsinstalled(memcache_client, appsinstalled, dry)
-    if not ok:
+    all_ok: bool = insert_appsinstalled(memcache_client, appsinstalled, dry)
+    if not all_ok:
         return ProcessingStatus.ERROR
 
     return ProcessingStatus.OK
 
 
 def process_file(fn: str, memcache_addresses: List[str], dry: bool) -> None:
+    """
+    Process single file by memcache
+    """
     worker = mp.current_process()
     logging.info(f"[{worker.name}] Processing {fn}")
 
@@ -93,7 +107,7 @@ def process_file(fn: str, memcache_addresses: List[str], dry: bool) -> None:
         memcache_addresses,
         socket_timeout=3,
         dead_retry=MEMCACHE_RETRY_TIMEOUT_SECONDS,
-    )
+        )
     with gzip.open(fn) as fd:
         job = partial(process_line, memcache_client=memcache_client, dry=dry)
         statuses = Counter(map(job, fd))
@@ -108,33 +122,34 @@ def process_file(fn: str, memcache_addresses: List[str], dry: bool) -> None:
         logging.info(
             f"[{worker.name}] [{fn}] Acceptable error rate: {err_rate}."
             f" Successfull load"
-        )
+            )
     else:
         logging.error(
             f"[{worker.name}] [{fn}] High error rate: "
             f"{err_rate} > {NORMAL_ERR_RATE}. Failed load"
-        )
+            )
 
     return fn
 
 
 def main(options):
-    """ Entry point
+    """
+    Entry point
     """
     memcache_addresses: List[str] = [
         options.idfa,
         options.gaid,
         options.adid,
         options.dvid,
-    ]
+        ]
 
     job = partial(
         process_file, memcache_addresses=memcache_addresses, dry=options.dry
-    )
+        )
 
     files = sorted(
         glob.glob(options.pattern), key=lambda file: Path(file).name
-    )
+        )
 
     with mp.Pool() as pool:
         for processed_file in pool.imap(job, files):
@@ -144,6 +159,9 @@ def main(options):
 
 
 if __name__ == "__main__":
+    """
+    Starts here
+    """
     op = OptionParser()
 
     op.add_option("-l", "--log", action="store", default=None)
@@ -151,7 +169,7 @@ if __name__ == "__main__":
     op.add_option("--loglevel", action="store", default="INFO")
     op.add_option(
         "--pattern", action="store", default="/data/appsinstalled/*.tsv.gz"
-    )
+        )
     op.add_option("--idfa", action="store", default="127.0.0.1:33013")
     op.add_option("--gaid", action="store", default="127.0.0.1:33014")
     op.add_option("--adid", action="store", default="127.0.0.1:33015")
@@ -164,7 +182,7 @@ if __name__ == "__main__":
         level=getattr(logging, opts.loglevel, logging.INFO),
         format="[%(asctime)s] %(levelname).1s %(message)s",
         datefmt="%Y.%m.%d %H:%M:%S",
-    )
+        )
 
     logging.info("Memc loader started with options: %s" % opts)
 
